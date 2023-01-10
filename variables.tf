01f1332831452ceabad1efbf822ec2f6d5393720
Dan Barr <dan@dbarr.com>
2023-01-10 14:29:51 -0500

    Update variable defaults

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "prefix" {
 variable "region" {
   type        = string
   description = "The region where the resources are created."
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 
 variable "env" {
