a031d62688f402085522e4cc8211f95e1cf76b76
Dan Barr <dan@dbarr.com>
2023-06-04 13:46:43 -0400

    Update dependencies again
    * On second thought, no-code modules should be more explicit since
      there's no parent module to enforce.
    * Add TF version

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 terraform {
+  required_version = ">= 0.13"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.0"
+      version = "~> 5.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.4"
+      version = "~> 3.4"
     }
   }
 }
